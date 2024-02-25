2dd753b12fdfea560befc5c18c9f6839a3f0a023
jaga07u <pradha02jagadish@gmail.com>
2024-02-25 12:20:29 +0530

    Add healthChekcup controller

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import healthcheckRouter from "./routes/healtcheck.routes.js"
 
 
 const app=express();
@@ -31,7 +32,8 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlist", playlistRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 
  export {app}
